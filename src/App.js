ad1cd70624982d6865c60fe401f999d777512e1e
ilhamhafizha <muhammadilhamhafizha@gmail.com>
2025-06-24 17:48:38 +0700

    feat: Implement core To-Do list features
    This commit introduces the fundamental functionalities required for a basic To-Do application:
    - Add new To-Do items with unique IDs and default 'uncompleted' status.
    - Toggle completion status of individual To-Do items.
    - Filter To-Do items by 'all', 'completed', and 'uncompleted' states.
    - Display dynamic To-Do list based on current filters and state.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [filter, setFilter] = useState("all");
+
+  const handleClick = () => {
+    setTodos((currrentTodos) => [
+      ...currrentTodos,
+      {
+        id: currrentTodos.length + 1,
+        title: `TODO ${currrentTodos.length + 1}`,
+        complated: false,
+      },
+    ]);
+  };
+
+  const handleClickItemClick = (id) => {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, complated: !todo.complated };
+        }
+        return todo;
+      });
+    });
+  };
+
+  const filterTodo = (todo, filter) => {
+    if (filter === "all") {
+      return true;
+    }
+    if (filter === "complated") {
+      return todo.complated;
+    }
+    if (filter === "uncomplated") {
+      return !todo.complated;
+    }
+  };
+
+  const visibleTodos = todos.filter((todo) => filterTodo(todo, filter));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Todo App</h1>
+      <button onClick={handleClick}>Add Todo</button>
+      <button onClick={() => setFilter("all")}>Show all</button>
+      <button onClick={() => setFilter("complated")}>Show Complated Todo</button>
+      <button onClick={() => setFilter("uncomplated")}>Show Uncomplated Todo</button>
+
+      <div>
+        <h2>TodoList</h2>
+        {visibleTodos.map((todo) => {
+          return (
+            <div key={todo.id}>
+              <input type="checkbox" checked={todo.complated} onChange={() => handleClickItemClick(todo.id)} />
+              <span>{todo.title}</span>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
